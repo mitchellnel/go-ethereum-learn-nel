1628c9aa87930fce7b0204dc012dfca36686ed2a
mitchellnel <mitchellmclin@gmail.com>
2022-07-04 23:20:27 +0800

    Refactoring deployment into function

--- todo-list-smart-contract/main.go
+++ todo-list-smart-contract/main.go
@@ -41,52 +41,11 @@ func main() {
 		"wallet/UTC--2022-07-02T16-44-15.195452000Z--d8eaef6563d22583bf98759088d3044c16c864c1",
 	)
 
-	// prepare deployment txn data
-	// we need: nonce, gas limit, gas price, and chain ID
-	nonce_uint64, err := client.PendingNonceAt(context.Background(), pubAddr)
-	if err != nil {
-		log.Fatalf("Error getting nonce for %v: %v", pubAddr.Hex(), err)
-	}
-	var nonce big.Int
-	nonce.SetUint64(nonce_uint64)
-
-	var gasLimit uint64 = 3_000_000
-
-	gasPrice, err := client.SuggestGasPrice(context.Background())
-	if err != nil {
-		log.Fatalf("Error getting suggested gas price: %v", err)
-	}
-
-	chainID, err := client.ChainID(context.Background())
-	if err != nil {
-		log.Fatalf("Error getting chain ID from ethclient: %v", err)
-	}
-
-	// create transaction options object
-	auth, err := bind.NewKeyedTransactorWithChainID(&pvk, chainID)
-	if err != nil {
-		log.Fatalf("Error creating transaction signer: %v", err)
-	}
-
-	auth.From = pubAddr
-	auth.Nonce = &nonce
-	auth.GasLimit = gasLimit
-	auth.GasPrice = gasPrice
-
-	// use generated code from abigen in todo.go to deploy the contract
-	contractAddr, txn, _, err := todo.DeployTodo(auth, client)
-	if err != nil {
-		log.Fatalf("Error deploying Todo contract: %v", err)
-	} else {
-		log.Printf(
-			"Successfully deployed Todo contract -- transaction hash is %v",
-			txn.Hash().Hex(),
-		)
-		log.Printf("Todo contract address is %v\n", common.HexToAddress(contractAddr.Hex()))
-	}
+	// deploy the Todo contract
+	deployContract(client, pvk, &pubAddr)
 }
 
-func parseKeystore(keystoreFile string) (ecdsa.PrivateKey, ecdsa.PublicKey, common.Address) {
+func parseKeystore(keystoreFile string) (*ecdsa.PrivateKey, *ecdsa.PublicKey, common.Address) {
 	// get key from wallet
 	keystoreBytes, err := ioutil.ReadFile(keystoreFile)
 	if err != nil {
@@ -105,5 +64,51 @@ func parseKeystore(keystoreFile string) (ecdsa.PrivateKey, ecdsa.PublicKey, comm
 	// derive public address
 	pubAddr := crypto.PubkeyToAddress(pbk)
 
-	return *pvk, pbk, pubAddr
+	return pvk, &pbk, pubAddr
+}
+
+func deployContract(client *ethclient.Client, pvk *ecdsa.PrivateKey, ownerAddress *common.Address) {
+	// prepare deployment txn data
+	// we need: nonce, gas limit, gas price, and chain ID
+	nonce_uint64, err := client.PendingNonceAt(context.Background(), *ownerAddress)
+	if err != nil {
+		log.Fatalf("Error getting nonce for %v: %v", ownerAddress.Hex(), err)
+	}
+	var nonce big.Int
+	nonce.SetUint64(nonce_uint64)
+
+	var gasLimit uint64 = 3_000_000
+
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatalf("Error getting suggested gas price: %v", err)
+	}
+
+	chainID, err := client.ChainID(context.Background())
+	if err != nil {
+		log.Fatalf("Error getting chain ID from ethclient: %v", err)
+	}
+
+	// create transaction options object
+	auth, err := bind.NewKeyedTransactorWithChainID(pvk, chainID)
+	if err != nil {
+		log.Fatalf("Error creating transaction signer: %v", err)
+	}
+
+	auth.From = *ownerAddress
+	auth.Nonce = &nonce
+	auth.GasLimit = gasLimit
+	auth.GasPrice = gasPrice
+
+	// use generated code from abigen in todo.go to deploy the contract
+	contractAddr, txn, _, err := todo.DeployTodo(auth, client)
+	if err != nil {
+		log.Fatalf("Error deploying Todo contract: %v", err)
+	} else {
+		log.Printf(
+			"Successfully deployed Todo contract -- transaction hash is %v",
+			txn.Hash().Hex(),
+		)
+		log.Printf("Todo contract address is %v\n", common.HexToAddress(contractAddr.Hex()))
+	}
 }
