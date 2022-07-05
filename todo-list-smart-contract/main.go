6ca29dbfc91e49e29e1d992ff9577e6dead2c44a
mitchellnel <mitchellmclin@gmail.com>
2022-07-05 18:36:06 +0800

    Add contract interaction code - also add lines to wait for blocks to be mined to ensure proper contract interaction

--- todo-list-smart-contract/main.go
+++ todo-list-smart-contract/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto/ecdsa"
 	todo "example/go-ethereum-learn-nel/todo-list-smart-contract/gen"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"math/big"
@@ -12,6 +13,7 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/joho/godotenv"
@@ -42,7 +44,12 @@ func main() {
 	)
 
 	// deploy the Todo contract
-	deployContract(client, pvk, &pubAddr)
+	contractAddr, _, _ := deployContract(client, pvk, &pubAddr)
+
+	// interact with the todo contract
+	// we could already pass the contract object, but to demonstrate creating an instance of the
+	// object bound to an on-chain contract, we'll just pass the address
+	interactWithContract(client, pvk, pubAddr, contractAddr)
 }
 
 func parseKeystore(keystoreFile string) (*ecdsa.PrivateKey, *ecdsa.PublicKey, common.Address) {
@@ -67,7 +74,11 @@ func parseKeystore(keystoreFile string) (*ecdsa.PrivateKey, *ecdsa.PublicKey, co
 	return pvk, &pbk, pubAddr
 }
 
-func deployContract(client *ethclient.Client, pvk *ecdsa.PrivateKey, ownerAddress *common.Address) {
+func deployContract(
+	client *ethclient.Client,
+	pvk *ecdsa.PrivateKey,
+	ownerAddress *common.Address,
+) (common.Address, types.Transaction, todo.Todo) {
 	// prepare deployment txn data
 	// we need: nonce, gas limit, gas price, and chain ID
 	nonce_uint64, err := client.PendingNonceAt(context.Background(), *ownerAddress)
@@ -92,7 +103,7 @@ func deployContract(client *ethclient.Client, pvk *ecdsa.PrivateKey, ownerAddres
 	// create transaction options object
 	auth, err := bind.NewKeyedTransactorWithChainID(pvk, chainID)
 	if err != nil {
-		log.Fatalf("Error creating transaction signer: %v", err)
+		log.Fatalf("Error creating transaction signer and options: %v", err)
 	}
 
 	auth.From = *ownerAddress
@@ -101,7 +112,7 @@ func deployContract(client *ethclient.Client, pvk *ecdsa.PrivateKey, ownerAddres
 	auth.GasPrice = gasPrice
 
 	// use generated code from abigen in todo.go to deploy the contract
-	contractAddr, txn, _, err := todo.DeployTodo(auth, client)
+	contractAddr, txn, contract, err := todo.DeployTodo(auth, client)
 	if err != nil {
 		log.Fatalf("Error deploying Todo contract: %v", err)
 	} else {
@@ -111,4 +122,93 @@ func deployContract(client *ethclient.Client, pvk *ecdsa.PrivateKey, ownerAddres
 		)
 		log.Printf("Todo contract address is %v\n", common.HexToAddress(contractAddr.Hex()))
 	}
+
+	// wait for deployment transaction to be mined
+	_, err = bind.WaitMined(context.Background(), client, txn)
+	if err != nil {
+		log.Fatalf("Error mining Todo deployment transaction: %v", err)
+	}
+
+	return contractAddr, *txn, *contract
+}
+
+func interactWithContract(
+	client *ethclient.Client,
+	pvk *ecdsa.PrivateKey,
+	pubAddr common.Address,
+	contractAddr common.Address,
+) {
+	// sanitise addresses
+	pubAddr = common.HexToAddress(pubAddr.Hex())
+	contractAddr = common.HexToAddress(contractAddr.Hex())
+
+	// create an instance of the Todo contract, bound to the on-chain contract
+
+	contract, err := todo.NewTodo(contractAddr, client)
+	if err != nil {
+		log.Fatalf("Error creating an instance of the Todo contract: %v", err)
+	}
+
+	// then, we can access the functions of our smart contract by calling contract.FunctionName
+	// e.g. contract.AddTask
+
+	// suppose we wanted to add a task by calling AddTask
+	// we need to provide contract.AddTask with: transaction options, and the actual parameter the
+	// smart contract function needs to be passed, i.e. _content
+
+	// prepare transcation options
+	chainID, err := client.ChainID(context.Background())
+	if err != nil {
+		log.Fatalf("Error getting chaind ID from ethclient: %v", err)
+	}
+
+	var gasLimit uint64 = 3_000_000
+
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatalf("Error getting suggested gas price: %v", err)
+	}
+
+	txnOpts_AddTask, err := bind.NewKeyedTransactorWithChainID(pvk, chainID)
+	if err != nil {
+		log.Fatalf("Error creating transaction signer and options: %v", err)
+	}
+	txnOpts_AddTask.GasLimit = gasLimit
+	txnOpts_AddTask.GasPrice = gasPrice
+
+	// then call the smart contract function that we want to execute
+	taskName := "Learn about go-ethereum"
+
+	txn_AddTask, err := contract.AddTask(txnOpts_AddTask, taskName)
+	if err != nil {
+		log.Fatalf("Error creating transaction for AddTask(): %v", err)
+	} else {
+		log.Printf(
+			"Successfully made transaction for AddTask() -- transaction has is %v",
+			txn_AddTask.Hash().Hex(),
+		)
+	}
+
+	// wait for transaction to be mined
+	_, err = bind.WaitMined(context.Background(), client, txn_AddTask)
+	if err != nil {
+		log.Fatalf("Error mining AddTask() transaction: %v", err)
+	}
+
+	// then, suppose we wanted to get a list of our todo tasks by calling ListTasks
+	// note that in ListTasks, we are not changing the state of the blockchain -- just reading it
+	// this means that we will make a **call** not a transaction
+
+	// so, we need not sign any transactions, we just make a call to the ListTasks function
+	// (passing some caller options)
+	tasks, err := contract.ListTasks(&bind.CallOpts{
+		From: pubAddr,
+	})
+	if err != nil {
+		log.Fatalf("Error making call to ListTasks(): %v", err)
+	} else {
+		log.Printf("Successfully made call to ListTasks()")
+	}
+
+	fmt.Println("Current Tasks:\n\t", tasks)
 }
